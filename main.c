f787bca1ce96a43af9655719d8f13ad1033e6d87
Souleymane <souleymane11dembele@gmail.com>
2023-12-03 18:15:15 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+/**
+ * File              : main.c
+ * Author            : Souleymane Dembele <sdembele@uw.edu>
+ * Date              : 12.03.2023
+ * Last Modified Date: 12.03.2023
+ * Last Modified By  : Souleymane Dembele <sdembele@uw.edu>
+ */
+
+int main(void) { return 0; }
